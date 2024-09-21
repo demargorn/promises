b745ed93fb5d487e62ec48a5ecb433e28fc81c51
Михаил Дорошин <demargorn@gmail.com>
2024-09-21 23:33:10 +0300

    changes in task added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
-import './GameSavingLoader';
+import { GameSavingLoader } from './GameSavingLoader';
+
+GameSavingLoader.load()
+   .then((saving) => console.log(saving))
+   .catch((err) => console.log(err));
